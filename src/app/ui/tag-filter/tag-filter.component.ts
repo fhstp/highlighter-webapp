c352bc35e731832fbde1eed08d449b94fedbb26f
doomsayer2 <fgrassinger@gmx.at>
2018-11-26 15:24:34 +0100

    Adds first boilerplate that works for the tag filter line. Currently with non real data but the structure is ready. (refs #37)

--- src/app/ui/tag-filter/tag-filter.component.ts
+++ src/app/ui/tag-filter/tag-filter.component.ts
@@ -17,18 +17,100 @@ export class TagFilterComponent implements OnInit {
 
   rulesClean: string[];
 
-  constructor(private dataStorage: DataStorageService) {}
+  // This is the desired structure...
+  finalTerms: any;
+  termsSelected: any;
+
+  constructor(private dataStorage: DataStorageService) {
+    // Just filling the desired structure and build it later on...
+    this.finalTerms = [
+      {
+        name: 'Garantie',
+        color: 'rgb(157, 18, 186)',
+        fontC: 'rgb(255, 255, 255)',
+        group: 'Garantie',
+        occ: 7,
+        isMain: true
+      },
+      {
+        name: 'Mangel',
+        color: 'rgb(157, 18, 186)',
+        fontC: 'rgb(255, 255, 255)',
+        group: 'Garantie',
+        occ: 2,
+        isMain: false
+      },
+      {
+        name: 'Gewähr',
+        color: 'rgb(157, 18, 186)',
+        fontC: 'rgb(255, 255, 255)',
+        group: 'Garantie',
+        occ: 15,
+        isMain: false
+      },
+      {
+        name: 'Gewährleistung',
+        color: 'rgb(157, 18, 186)',
+        fontC: 'rgb(255, 255, 255)',
+        group: 'Garantie',
+        occ: 3,
+        isMain: false
+      },
+      {
+        name: 'Umtausch',
+        color: 'rgb(244, 199, 65)',
+        fontC: 'rgb(0, 0, 0)',
+        group: 'Umtausch',
+        occ: 3,
+        isMain: false
+      },
+      {
+        name: 'Mangel',
+        color: 'rgb(244, 199, 65)',
+        fontC: 'rgb(0, 0, 0)',
+        group: 'Umtausch',
+        occ: 3,
+        isMain: false
+      },
+      {
+        name: 'Beschädigung',
+        color: 'rgb(244, 199, 65)',
+        fontC: 'rgb(0, 0, 0)',
+        group: 'Umtausch',
+        occ: 3,
+        isMain: false
+      },
+      {
+        name: 'test',
+        color: 'rgb(199, 255, 33)',
+        fontC: 'rgb(0, 0, 0)',
+        group: 'Andere',
+        occ: 2,
+        isMain: true
+      },
+      {
+        name: 'paypal',
+        color: 'rgb(65, 244, 193)',
+        fontC: 'rgb(0, 0, 0)',
+        group: 'Andere',
+        occ: 2,
+        isMain: true
+      }
+    ];
+  }
 
   ngOnInit() {
     // Combine both observable streams together and wait till both "finished" with zip
     this.tagFilterData = zip(
       this.dataStorage.currentData,
-      this.dataStorage.currentColors
+      this.dataStorage.currentColors,
+      this.dataStorage.searchTerms
     )
       .pipe(
-        map(([first, second]) => {
+        map(([first, second, third]) => {
+          console.log('all: ', first, second, third);
           return {
-            searchTerms: first.searchTerms,
+            searchTerms: third,
             foundOcc: first.found_occurences,
             styleRules: second
           };
@@ -40,13 +122,44 @@ export class TagFilterComponent implements OnInit {
   }
 
   assembleTags(data: any) {
-    this.searchTerms = data.searchTerms;
+    // Object containing all main categories and their sub categories as array
+    const searchTerms = data.searchTerms;
+
+    // First extract the main categories from the search terms property
+    let mainCategories: Array<string>;
+    mainCategories = Object.keys(searchTerms);
+
+    // this.searchTerms = data.searchTerms;
     this.foundOcc = data.foundOcc;
     this.styleRules = data.styleRules;
 
-    this.rulesClean = this.searchTerms.map(e => {
-      const rule = this.styleRules.get('.' + e.toLowerCase());
-      return rule;
-    });
+    // this.rulesClean = this.searchTerms.map(e => {
+    //   const rule = this.styleRules.get('.' + e.toLowerCase());
+    //   return rule;
+    // });
+  }
+
+  /**
+   * Executes on adding an elment to the selected list
+   * @param $event is the object we currently add
+   */
+  onAdd($event) {
+    console.log('Added: ', $event);
+  }
+
+  /**
+   * Executes on removing an element from the selected list
+   * @param $event is the object we currently remove
+   */
+  onRemove($event) {
+    console.log('Removed: ', $event);
+  }
+
+  /**
+   * Method is called once all tags are removed from the selected list.
+   * If this is called then no entries are selected.
+   */
+  onClear() {
+    console.log('All removed');
   }
 }
